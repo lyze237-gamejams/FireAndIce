d37f4bd05e78b9a014222c4249ed6c1f7901ed9d
Lyze <11274700+lyze237@users.noreply.github.com>
2020-09-21 20:34:22 +0200

    Adjusted box colors

--- core/src/main/java/dev/lyze/parallelworlds/screens/game/entities/PlayerColor.java
+++ core/src/main/java/dev/lyze/parallelworlds/screens/game/entities/PlayerColor.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.graphics.Color;
 import lombok.Getter;
 
 public enum PlayerColor {
-    Red(Color.RED), Blue(Color.BLUE);
+    Red(new Color(228 / 256f, 59 / 256f, 68 / 256f, 1f)), Blue(new Color(0f, 149 / 256f, 233 / 256f, 1f));
 
     @Getter
     private final Color renderColor;
