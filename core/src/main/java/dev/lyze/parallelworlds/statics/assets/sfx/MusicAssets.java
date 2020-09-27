51215e546bac39c98630aeb2ac14ed21f9d94ba0
Lyze <11274700+lyze237@users.noreply.github.com>
2020-09-27 20:45:08 +0200

    Switched to mp3s

--- core/src/main/java/dev/lyze/parallelworlds/statics/assets/sfx/MusicAssets.java
+++ core/src/main/java/dev/lyze/parallelworlds/statics/assets/sfx/MusicAssets.java
@@ -7,25 +7,25 @@ import dev.lyze.parallelworlds.statics.utils.LoadAssetFromFile;
 import lombok.Getter;
 
 public class MusicAssets extends DynamicAssets {
-    @Getter @LoadAssetFromFile("music/BoosterLevel_1.ogg")
+    @Getter @LoadAssetFromFile("music/BoosterLevel_1.mp3")
     private Music booster;
 
-    @Getter @LoadAssetFromFile("music/8-Bit-Puzzler.ogg")
+    @Getter @LoadAssetFromFile("music/8-Bit-Puzzler.mp3")
     private Music eightBitPuzzler;
 
-    @Getter @LoadAssetFromFile("music/Arcade-Puzzler_v001.ogg")
+    @Getter @LoadAssetFromFile("music/Arcade-Puzzler_v001.mp3")
     private Music arcadePuzzler;
 
-    @Getter @LoadAssetFromFile("music/Caffeine-Crazed-Coin-Op-Kids.ogg")
+    @Getter @LoadAssetFromFile("music/Caffeine-Crazed-Coin-Op-Kids.mp3")
     private Music caffeine;
 
-    @Getter @LoadAssetFromFile("music/Cute-8-Bit-Monsters.ogg")
+    @Getter @LoadAssetFromFile("music/Cute-8-Bit-Monsters.mp3")
     private Music monsters;
 
-    @Getter @LoadAssetFromFile("music/Pixel-Island.ogg")
+    @Getter @LoadAssetFromFile("music/Pixel-Island.mp3")
     private Music pixelIsland;
 
-    @Getter @LoadAssetFromFile("music/Up-the-Ladder.ogg")
+    @Getter @LoadAssetFromFile("music/Up-the-Ladder.mp3")
     private Music upTheLadder;
 
     public MusicAssets(AssetManager assMan) {
@@ -33,6 +33,6 @@ public class MusicAssets extends DynamicAssets {
     }
 
     public Music get(String name) {
-        return getAssMan().get("music/" + name, Music.class);
+        return getAssMan().get("music/" + name + ".mp3", Music.class);
     }
 }
--- core/src/main/java/dev/lyze/parallelworlds/statics/assets/sfx/SoundAssets.java
+++ core/src/main/java/dev/lyze/parallelworlds/statics/assets/sfx/SoundAssets.java
@@ -9,55 +9,55 @@ import lombok.Getter;
 import java.util.Random;
 
 public class SoundAssets extends DynamicAssets {
-    @Getter @LoadAssetFromFile("sounds/Action Misc 8.ogg")
+    @Getter @LoadAssetFromFile("sounds/Action Misc 8.mp3")
     private Sound actionMisc8;
 
-    @Getter @LoadAssetFromFile("sounds/Coin 1.ogg")
+    @Getter @LoadAssetFromFile("sounds/Coin 1.mp3")
     private Sound coin1;
 
-    @Getter @LoadAssetFromFile("sounds/Coin 2.ogg")
+    @Getter @LoadAssetFromFile("sounds/Coin 2.mp3")
     private Sound coin2;
 
-    @Getter @LoadAssetFromFile("sounds/Coin Total Win 2.ogg")
+    @Getter @LoadAssetFromFile("sounds/Coin Total Win 2.mp3")
     private Sound coinTotalWin2;
 
-    @Getter @LoadAssetFromFile("sounds/Coin Total Win.ogg")
+    @Getter @LoadAssetFromFile("sounds/Coin Total Win.mp3")
     private Sound coinTotalWin1;
 
-    @Getter @LoadAssetFromFile("sounds/Extra Life.ogg")
+    @Getter @LoadAssetFromFile("sounds/Extra Life.mp3")
     private Sound extraLife;
 
-    @Getter @LoadAssetFromFile("sounds/Magic Dark.ogg")
+    @Getter @LoadAssetFromFile("sounds/Magic Dark.mp3")
     private Sound magicDark;
 
-    @Getter @LoadAssetFromFile("sounds/Power Up 1.ogg")
+    @Getter @LoadAssetFromFile("sounds/Power Up 1.mp3")
     private Sound powerUp1;
 
-    @Getter @LoadAssetFromFile("sounds/Power Up 2.ogg")
+    @Getter @LoadAssetFromFile("sounds/Power Up 2.mp3")
     private Sound powerUp2;
 
-    @Getter @LoadAssetFromFile("sounds/Power Up 4.ogg")
+    @Getter @LoadAssetFromFile("sounds/Power Up 4.mp3")
     private Sound powerUp4;
 
-    @Getter @LoadAssetFromFile("sounds/Power Up 5.ogg")
+    @Getter @LoadAssetFromFile("sounds/Power Up 5.mp3")
     private Sound powerUp5;
 
-    @Getter @LoadAssetFromFile("sounds/Sword 1.ogg")
+    @Getter @LoadAssetFromFile("sounds/Sword 1.mp3")
     private Sound sword1;
 
-    @Getter @LoadAssetFromFile("sounds/Sword 2.ogg")
+    @Getter @LoadAssetFromFile("sounds/Sword 2.mp3")
     private Sound sword2;
 
-    @Getter @LoadAssetFromFile("sounds/Jump 1.ogg")
+    @Getter @LoadAssetFromFile("sounds/Jump 1.mp3")
     private Sound jump1;
 
-    @Getter @LoadAssetFromFile("sounds/Jump 3.ogg")
+    @Getter @LoadAssetFromFile("sounds/Jump 3.mp3")
     private Sound jump3;
 
-    @Getter @LoadAssetFromFile("sounds/Fall.ogg")
+    @Getter @LoadAssetFromFile("sounds/Fall.mp3")
     private Sound fall;
 
-    @Getter @LoadAssetFromFile("sounds/Long Slide Up.ogg")
+    @Getter @LoadAssetFromFile("sounds/Long Slide Up.mp3")
     private Sound longSlideUp;
 
     private final Random random = new Random();
